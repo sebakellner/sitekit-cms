1ff323c50a4e576a5374382f6dc2912bdc9b3c69
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-12 17:39:54 -0300

    Add PanelBox component for consistent layout styling in PanelComponentSelector

--- src/components/cms/editor/PanelComponentSelector.tsx
+++ src/components/cms/editor/PanelComponentSelector.tsx
@@ -1,9 +1,10 @@
 import { Box, Heading, Text, TextInput } from 'grommet'
 import { Search } from 'grommet-icons'
+import PanelBox from '../ui/PanelBox'
 
 const ComponentSelectorItem = () => {
   return (
-    <Box gap="small" round="small" flex="grow" hoverIndicator>
+    <Box gap="small" round="small" flex="grow" hoverIndicator={true}>
       <Box
         overflow="hidden"
         height="xsmall"
@@ -25,25 +26,27 @@ const ComponentSelectorItem = () => {
 const PanelComponentSelector = () => {
   return (
     <Box justify="start">
-      <Box
-        pad={{ horizontal: 'medium', vertical: 'medium' }}
-        gap="xsmall"
-        flex="grow"
-        border={{ side: 'bottom', color: 'dark-3' }}
-      >
+      <PanelBox>
         <Heading level={5} margin="none">
           UI Section Components
         </Heading>
         <Text size="xsmall" color="dark-4">
           Select a component to add to your layout
         </Text>
-      </Box>
-      <Box pad="medium" gap="small" flex="grow">
-        <TextInput icon={<Search />} placeholder="Search" dropHeight="small" />
-      </Box>
+      </PanelBox>
+
+      <PanelBox borderSide={false}>
+        <TextInput
+          icon={<Search />}
+          size="small"
+          placeholder="Search"
+          dropHeight="small"
+        />
+      </PanelBox>
+
       <Box
         pad="medium"
-        gap="large"
+        gap="medium"
         overflow={{ vertical: 'scroll' }}
         className="custom-scrollbar"
       >
