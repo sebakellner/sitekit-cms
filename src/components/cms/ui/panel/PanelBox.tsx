2d378fd46269046e4bfe0f78658106a38bd370c1
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-14 15:57:24 -0300

    Feature: Add PanelBox, PanelBoxCollapsable, PanelBoxScroll, and PanelTabs components for enhanced panel functionality

--- src/components/cms/ui/panel/PanelBox.tsx
+++ src/components/cms/ui/panel/PanelBox.tsx
@@ -3,24 +3,20 @@ import { Box } from 'grommet'
 export type PanelBoxProps = {
   children: React.ReactNode
   borderSide?: 'top' | 'bottom' | 'left' | 'right' | false
+  pad?: string | { horizontal?: string; vertical?: string }
 } & React.ComponentProps<typeof Box>
 
 const PanelBox = ({
   children,
   borderSide = 'bottom',
+  pad = 'medium',
   ...restProps
 }: PanelBoxProps) => {
   const borderProp = borderSide
     ? { side: borderSide, color: 'dark-3' }
     : undefined
   return (
-    <Box
-      pad={{ horizontal: 'medium', vertical: 'medium' }}
-      gap="xsmall"
-      flex="grow"
-      border={borderProp}
-      {...restProps}
-    >
+    <Box pad={pad} gap="xsmall" flex="grow" border={borderProp} {...restProps}>
       {children}
     </Box>
   )
