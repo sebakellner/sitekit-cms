1deb6af2735a8ec7d73af822e779d009379f4797
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-14 16:12:16 -0300

    Feature: Add Header, Footer, and Sidebar components for improved layout structure

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,6 +1,6 @@
 import { Grid, Box } from 'grommet'
-import Header from '@components/cms/Header'
-import Footer from '@components/cms/Footer'
+import Header from '@components/cms/ui/Header'
+import Footer from '@components/cms/ui/Footer'
 
 interface DefaultLayoutProps {
   children?: React.ReactNode
