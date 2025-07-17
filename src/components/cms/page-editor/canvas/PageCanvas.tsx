d60bee5da65143eaab01e26c3d74015586396335
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-17 16:48:44 -0300

    Feature/add PageCanvas component to EditorLayout

---
+++ src/components/cms/page-editor/canvas/PageCanvas.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from 'grommet'
+
+const PageCanvas = () => {
+  return (
+    <Box fill background="white">
+      <Text>Page Canvas Component</Text>
+    </Box>
+  )
+}
+
+export default PageCanvas
