3d0ea000a05b2958ed182b56160378961c6a80d6
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-18 10:16:16 -0300

    Feature/update PageCanvas component layout and structure

--- src/components/cms/page-editor/canvas/PageCanvas.tsx
+++ src/components/cms/page-editor/canvas/PageCanvas.tsx
@@ -1,9 +1,23 @@
-import { Box, Text } from 'grommet'
+import CardGrid from '@components/site/CardGrid'
+import Footer from '@components/site/Footer'
+import Hero from '@components/site/Hero'
+import InfoText from '@components/site/InfoText'
+import Nav from '@components/site/Nav'
+import { Box } from 'grommet'
 
 const PageCanvas = () => {
   return (
-    <Box fill background="white">
-      <Text>Page Canvas Component</Text>
+    <Box
+      style={{ display: 'block' }}
+      fill
+      background="white"
+      overflow={{ vertical: 'auto', horizontal: 'hidden' }}
+    >
+      <Nav />
+      <Hero />
+      <CardGrid />
+      <InfoText />
+      <Footer />
     </Box>
   )
 }
