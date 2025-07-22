6193b29ad2686e61e9331ff4b562a6a01dd2fa3f
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-22 11:28:34 -0300

    Refactor: update CardGridMeta to use the correct preview image

--- src/components/site/CardGrid/CardGrid.meta.ts
+++ src/components/site/CardGrid/CardGrid.meta.ts
@@ -1,11 +1,12 @@
 import CardGrid from '.'
+import preview from './cardgrid-preview.png'
 
 const CardGridMeta = {
   id: 'CardGrid',
   name: 'CardGrid',
   category: 'Section',
   description: 'A grid of cards for features or content.',
-  preview: './footer-preview.png',
+  preview,
   component: CardGrid,
 }
 
