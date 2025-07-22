32ab23977dada180d09cc4e584cd202be4d51338
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-22 12:10:20 -0300

    Refactor: update preview handling in ElementSelectorItem for improved background logic

--- src/components/cms/page-editor/panels/sidebar/selector/ElementSelectorItem.tsx
+++ src/components/cms/page-editor/panels/sidebar/selector/ElementSelectorItem.tsx
@@ -8,16 +8,31 @@ export type ElementSelectorItemProps = {
 
 const ElementSelectorItem: React.FC<ElementSelectorItemProps> = ({
   title,
-  preview = 'dark-3',
+  preview = '',
 }) => {
+  const hasImage =
+    preview &&
+    (preview.endsWith('.png') ||
+      preview.endsWith('.jpg') ||
+      preview.startsWith('data:'))
+  const previewBackground = hasImage
+    ? { image: `url(${preview})` }
+    : { color: 'dark-3' }
+
   return (
-    <Box gap="xsmall" round="small" flex="grow" margin={{ top: 'xsmall' }}>
+    <Box
+      gap="xsmall"
+      round="small"
+      flex="grow"
+      margin={{ top: 'xsmall' }}
+      hoverIndicator
+    >
       <Box
         overflow="hidden"
         height="130px"
-        background={{ image: `url(${preview})` }}
+        background={previewBackground}
         round="small"
-      ></Box>
+      />
       <Box gap="xsmall" pad="xsmall">
         <Text size="small" color="light-4">
           {title}
