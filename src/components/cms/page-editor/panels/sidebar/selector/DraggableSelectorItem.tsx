8f0792093d89effa8a59e17235db9eb740bcfc42
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-23 12:47:57 -0300

    Feature: add DraggableSelectorItem component for drag-and-drop functionality

---
+++ src/components/cms/page-editor/panels/sidebar/selector/DraggableSelectorItem.tsx
@@ -0,0 +1,29 @@
+import { useDraggable } from '@dnd-kit/core'
+
+interface DraggableSelectorItemProps {
+  id: string
+  children: React.ReactNode
+}
+
+const DraggableSelectorItem = ({
+  id,
+  children,
+}: DraggableSelectorItemProps) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: id,
+  })
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined
+
+  return (
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {children}
+    </div>
+  )
+}
+
+export default DraggableSelectorItem
