4b066f0d0e159577a27b31034373e260fd2d695b
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-23 17:19:21 -0300

    Feature: add Divider component for consistent UI separation

---
+++ src/components/cms/ui/Divider.tsx
@@ -0,0 +1,13 @@
+import { Box } from 'grommet'
+
+const Divider: React.FC = () => (
+  <Box
+    height="4px"
+    width="100%"
+    background="#1976d2"
+    margin={{ vertical: '2px' }}
+    round="xsmall"
+  />
+)
+
+export default Divider
