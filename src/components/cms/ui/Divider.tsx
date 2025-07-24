8759945f38baa0d592a61ab82463cf00749ae4e0
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-24 12:49:25 -0300

    Refactor: use BEM_BORDER_COLOR constant for background in Divider component

--- src/components/cms/ui/Divider.tsx
+++ src/components/cms/ui/Divider.tsx
@@ -1,10 +1,11 @@
 import { Box } from 'grommet'
+import { BEM_BORDER_COLOR } from '../page-editor/bem/constants'
 
 const Divider: React.FC = () => (
   <Box
     height="4px"
     width="100%"
-    background="#1976d2"
+    background={BEM_BORDER_COLOR}
     margin={{ vertical: '2px' }}
     round="xsmall"
   />
