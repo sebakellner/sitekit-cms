be808c349ca0b1e87d28b6c16110f28192cda384
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-26 18:24:39 -0300

    Refactor/add parameters to addSection for improved section management

--- src/features/editor/types/index.ts
+++ src/features/editor/types/index.ts
@@ -6,5 +6,9 @@ export type PageStore = {
   setSections: (sections: Section[]) => void
   updateSectionProps: (id: string, newProps: Record<string, unknown>) => void
   selectSection: (id: string | null) => void
-  addSection: (type: string, props?: Record<string, unknown>) => void
+  addSection: (
+    id: string,
+    name: string,
+    props?: Record<string, unknown>
+  ) => void
 }
