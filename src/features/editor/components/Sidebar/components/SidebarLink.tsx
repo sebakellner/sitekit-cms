5ae93898b20fa118a7aa95d2d4fc52d9eea1d09a
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-30 12:47:46 -0300

    Refactor: remove Tip component from SidebarLink for simplified rendering

--- src/features/editor/components/Sidebar/components/SidebarLink.tsx
+++ src/features/editor/components/Sidebar/components/SidebarLink.tsx
@@ -1,5 +1,4 @@
-import { TipContent } from '@components/cms/ui'
-import { Box, Button, Tip } from 'grommet'
+import { Box, Button } from 'grommet'
 import type { SidebarLinkProps } from './SidebarLink.types'
 
 const SidebarLink = ({
@@ -8,22 +7,17 @@ const SidebarLink = ({
   label,
   onClick,
 }: SidebarLinkProps & { onClick?: () => void }) => (
-  <Tip
-    plain
-    dropProps={{ align: { left: 'right' } }}
-    content={<TipContent>{label}</TipContent>}
-  >
-    <Button plain hoverIndicator={true} onClick={onClick}>
-      <Box
-        align="center"
-        gap="small"
-        pad="small"
-        background={active ? 'dark-3' : undefined}
-      >
-        {icon}
-      </Box>
-    </Button>
-  </Tip>
+  <Button plain hoverIndicator={true} onClick={onClick}>
+    <Box
+      align="center"
+      gap="small"
+      pad="small"
+      aria-label={label}
+      background={active ? 'dark-3' : undefined}
+    >
+      {icon}
+    </Box>
+  </Button>
 )
 
 export default SidebarLink
