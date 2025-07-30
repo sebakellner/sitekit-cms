e75f7d23601291db131bb7d0960d6b37bff49765
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-30 16:58:23 -0300

    feat: Refactor Sidebar component layout and update SidebarProps type

--- src/features/editor/components/Sidebar/Sidebar.types.ts
+++ src/features/editor/components/Sidebar/Sidebar.types.ts
@@ -1,9 +1,7 @@
-import type { PanelWrapper } from '@components/cms/ui'
-
 export type SidebarProps = {
   links?: {
     icon: React.ReactNode
     label: string
     active?: boolean
   }[]
-} & React.ComponentProps<typeof PanelWrapper>
+}
