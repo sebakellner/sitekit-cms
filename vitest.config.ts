63a0181467fe54d7b4e7d7605e0a474fd6c52ac2
Sebastian Kellner <seba.kellner@gmail.com>
2025-07-30 09:03:12 -0300

    chore: update .gitignore to include coverage directories and files
    feat: add @vitest/coverage-v8 dependency to package.json and package-lock.json
    
    fix: update vitest.config.ts to ensure coverage configuration is properly set

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './test/setup.ts',
+    coverage: {
+      exclude: ['**/*.types.ts', '**/index.ts', '**/*.meta.ts'],
+      enabled: true,
+      reporter: ['html', 'text'],
+    },
   },
 })
