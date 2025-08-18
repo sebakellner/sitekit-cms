19e9f0f832e655dc9a7135abba0a9384ee306736
Sebastian Kellner <seba.kellner@gmail.com>
2025-08-18 17:58:56 -0300

    Fix: Add props check in useGroupedPropsPanels to prevent errors

--- src/features/editor/hooks/useGroupedPropsPanels.ts
+++ src/features/editor/hooks/useGroupedPropsPanels.ts
@@ -22,6 +22,7 @@ export function useGroupedPropsPanels(
 ): GroupedPanel[] {
   return useMemo(() => {
     if (!meta) return []
+    if (!props) return []
 
     return (
       meta.panels?.map((panel) => ({
